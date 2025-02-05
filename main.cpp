eee3e39864a63347e643627cb13fa6dc14575f0b
Shaun Chong <data-miner00@users.noreply.github.com>
2025-02-05 20:28:14 +0800

    feat: Add ai mode flag for player2

--- main.cpp
+++ main.cpp
@@ -43,6 +43,13 @@ int main(void) {
     player2.height = PADDLE_HEIGHT;
     player2.speed = PLAYER2_PADDLE_SPEED;
 
+    AiPaddle player3{ state };
+    player3.x = SCREEN_WIDTH - PADDLE_WIDTH - 10;
+    player3.y = SCREEN_HEIGHT / 2 - PADDLE_HEIGHT / 2;
+    player3.width = PADDLE_WIDTH;
+    player3.height = PADDLE_HEIGHT;
+    player3.speed = PLAYER2_PADDLE_SPEED;
+
     char name[255];
     Rectangle nbounds = { SCREEN_WIDTH / 2 - 700 / 2, SCREEN_HEIGHT / 2 - 300 / 2, 700, 300 };
     const char* title = "Player name";
@@ -63,6 +70,8 @@ int main(void) {
     Color ballColor = WHITE;
     long timestamp = 0;
     bool blinkShow = true;
+    // make togglable
+    bool aiMode = false;
 
     while (!WindowShouldClose()) {
         if (IsKeyPressed(KEY_P)) {
@@ -144,13 +153,25 @@ int main(void) {
 
         ball.update();
         player1.update(ball.y);
-        player2.update();
+
+        if (!aiMode) {
+            player2.update();
+        } else {
+            player3.update(ball.y);
+        }
 
         if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player1.x, player1.y, player1.width, player1.height})) {
             ball.speed_x *= -1;
         }
-        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player2.x, player2.y, player2.width, player2.height})) {
-            ball.speed_x *= -1;
+
+        if (!aiMode) {
+            if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player2.x, player2.y, player2.width, player2.height})) {
+                ball.speed_x *= -1;
+            }
+        } else {
+            if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player3.x, player3.y, player3.width, player3.height})) {
+                ball.speed_x *= -1;
+            }
         }
 
         DrawLine(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT, WHITE);
@@ -158,7 +179,12 @@ int main(void) {
         ball.draw();
 
         player1.draw();
-        player2.draw();
+
+        if (!aiMode) {
+            player2.draw();
+        } else {
+            player3.draw();
+        }
 
         const char* player1_label = TextFormat("Player 1: %i", state.player1_score);
         const char* player2_label = TextFormat("%s: %i", name, state.player2_score);
@@ -169,7 +195,6 @@ int main(void) {
         DrawText(player1_label, SCREEN_WIDTH / 4 - player1_score_text_width / 2, 20, SCORE_FONT_SIZE, WHITE);
         DrawText(player2_label, SCREEN_WIDTH * 3 / 4 - player2_score_text_width / 2, 20, SCORE_FONT_SIZE, WHITE);
 
-        // respawn middle
         // acceleration
         // multi balls (how many)
         // extend length
